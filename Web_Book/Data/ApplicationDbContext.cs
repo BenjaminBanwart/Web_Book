b862e4eb7a98d757b34f917f5b01b606356dd063
Benjamin Banwart <BenjiBanwart@Gmail.com>
2023-04-01 12:36:50 -0500

    Added context model and migrated table to DB

---
+++ Web_Book/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_Book.Models;
+
+namespace Web_Book.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
