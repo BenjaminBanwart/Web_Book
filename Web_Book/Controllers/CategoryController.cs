1cebaac98f1af39f66ab547ac9823860007f7092
Benjamin Banwart <BenjiBanwart@Gmail.com>
2023-04-01 17:44:25 -0500

    Added success and error alerts on data operations

--- Web_Book/Controllers/CategoryController.cs
+++ Web_Book/Controllers/CategoryController.cs
@@ -39,6 +39,7 @@ namespace Web_Book.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
 
@@ -76,6 +77,7 @@ namespace Web_Book.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
 
@@ -98,6 +100,7 @@ namespace Web_Book.Controllers
 
             _db.Categories.Remove(categoryFromDb);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
     }
